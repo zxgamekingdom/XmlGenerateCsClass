22403d9943d7eca23aa931a6fdaa3a7429116d7c
Zhou Taurus <Zhou Taurus@Office>
2022-05-06 11:02:04 +0800

    将项目更改为类库

--- XmlGenerateCsClass/Program.cs
+++ XmlGenerateCsClass/Program.cs
@@ -1,16 +1,16 @@
-﻿#region
-
-using XmlGenerateCsClass;
-using XmlGenerateCsClass.CsharpClassInfos;
-using XmlGenerateCsClass.XmlInfos;
-
-#endregion
-
+﻿// #region
+//
+// using XmlGenerateCsClass;
+// using XmlGenerateCsClass.CsharpClassInfos;
+// using XmlGenerateCsClass.XmlInfos;
+//
+// #endregion
+//
+// // var xmlPath =
+// //     @"C:\Users\Zhou Taurus\source\repos\XmlGenerateCsClass\XmlGenerateCsClass\test.xml";
 // var xmlPath =
-//     @"C:\Users\Zhou Taurus\source\repos\XmlGenerateCsClass\XmlGenerateCsClass\test.xml";
-var xmlPath =
-    @"C:\Users\Zhou Taurus\Desktop\Deep-Learning\Classification\classify_pill_defects_deep_learning_1_preprocess.hdev";
-
-var xmlTree = await XmlTree.LoadFromFile(xmlPath);
-var csharpTree = CsharpTree.FromXmlTree(xmlTree);
-csharpTree.ToCsharpCode().WriteLine();
+//     @"C:\Users\Zhou Taurus\Desktop\Deep-Learning\Classification\classify_pill_defects_deep_learning_1_preprocess.hdev";
+//
+// var xmlTree = await XmlTree.LoadFromFile(xmlPath);
+// var csharpTree = CsharpTree.FromXmlTree(xmlTree);
+// csharpTree.ToCsharpCode().WriteLine();
